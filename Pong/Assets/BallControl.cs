2984635981171f2f0e1b27c21dc80aeb5e24af56
Lionnarta <lionnarta.savirandy2000@gmail.com>
2021-02-01 00:55:17 +0700

    Pong

---
+++ Pong/Assets/BallControl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D
+    private Rigidbody2D rigidBody2D;
+
+    // Initial force
+    public float xInitialForce;
+    public float yInitialForce;
+
+    // Debug
+    // Trajectory
+    private Vector2 trajectoryOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        trajectoryOrigin = transform.position;
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 consSpeed = GetComponent<Rigidbody2D>().velocity.normalized;
+        GetComponent<Rigidbody2D>().velocity = consSpeed * 10;
+    }
+
+    void ResetBall()
+    {
+        // Reset pot to 0,0
+        transform.position = Vector2.zero;
+
+        // Reset velocity to zero
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        // Randomize y initial force
+        float yRandomInitialForce = Random.Range(-yInitialForce,yInitialForce);
+
+        // Randomize direction
+        float randomDirection = Random.Range(0,2);
+
+        if(randomDirection < 1.0f)
+        {
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    void RestartGame()
+    {
+        // Return ball to initial pos
+        ResetBall();
+        // Give force to the ball after 2 second
+        Invoke("PushBall", 2);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ Pong/Assets/PlayerControl.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Button to move up
+    public KeyCode upButton = KeyCode.W;
+
+    // Button to move down
+    public KeyCode downButton = KeyCode.S;
+
+    // Movement speed
+    public float speed = 10.0f;
+
+    // Upper and lower boundary
+    public float yBoundary = 9.0f;
+
+    // Rigidbody 2d
+    private Rigidbody2D rigidBody2D;
+
+    // Score
+    private int score;
+
+    // Debug
+    // Last contact point
+    private ContactPoint2D lastContactPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get velocity
+        Vector2 velocity = rigidBody2D.velocity;
+
+        // Go up
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        // Go down
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        // No button clicked
+        else
+        {
+            velocity.y = 0.0f;
+        }
+        rigidBody2D.velocity = velocity;
+
+        // Get pos
+        Vector3 position = transform.position;
+
+        // Pos pass yBoundary
+        if(position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }
+        // Pos pass -yBoundary
+        else if(position.y < -yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+        transform.position = position;
+    }
+
+    // Raise score
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    // Reset score to 0
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    // Get score value
+    public int Score
+    {
+        get { return score; }
+    }
+
+    // Access last contact point
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+}
